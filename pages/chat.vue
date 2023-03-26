d3fcfeb60d0a0f8d5be1b861117a3d28d629a99f
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-03-26 09:50:50 +0200

    store template

--- pages/chat.vue
+++ pages/chat.vue
@@ -2,18 +2,20 @@
   <div>
     <input v-model="message" border="1 dark-400" rounded="xl" p="2" />
     <button p="2" @click="sendMessage" v-text="'Send'" />
-    <div>{{ data }}</div>
+    <div>{{ lastAnswer }}</div>
+    <div v-for="answer in answers" :key="answer">{{ answer }}</div>
   </div>
 </template>
 
 <script setup lang="ts">
-const { send } = useChatgpt()
-const data = ref('')
+const { send, lastAnswer, answers } = useStore()
 
 const message = ref('')
-async function sendMessage() {
-  const response = await send(message.value)
-  data.value = response
+
+// send message to store
+const sendMessage = () => {
+  send(message.value)
+  message.value = ''
 }
 </script>
 
--- stores/store.ts
+++ stores/store.ts
@@ -1,16 +1,29 @@
-export const useCounterStore = defineStore({
-    id: 'counter',
-    state: () => ({
-      count: 0,
-    }),
-    getters: {
-      doubleCount(): number {
-        return this.count * 2
-      },
-    },
-    actions: {
-      increment() {
-        this.count++
-      },
-    },
+import { defineStore } from 'pinia'
+
+export const useStore = defineStore('store', () => {
+  const { send } = useChatgpt()
+
+  // create a state that stores the answers returned from the chatbot
+  const state = reactive({
+    answers: [] as string[]
   })
+
+  // create a getter that returns the last answer
+  const getters = {
+    lastAnswer: computed(() => state.answers[state.answers.length - 1])
+  }
+
+  // create an action that sends a message to the chatbot and stores the answer
+  const actions = {
+    async send(message: string) {
+      const answer = await send(message)
+      state.answers.push(answer)
+    }
+  }
+
+  return {
+    ...toRefs(state),
+    ...getters,
+    ...actions
+  }
+})
